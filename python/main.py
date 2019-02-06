7809e9c71ee404646ffd74f7143282382272188f
Sergey Gorbatyuk <sergeygorbatyuk171@gmail.com>
2019-02-06 23:26:30 +0100

    Implemented a solver-wrapper CFPQ. On #9 issue.

--- python/main.py
+++ python/main.py
@@ -5,8 +5,33 @@ from math_utils import *
 VERBOSE = False
 
 def main(grammar_file, graph_file, args):
-    ...
+    grammar, inverse_grammar = parse_grammar(grammar_file)
+    graph, graph_size = parse_graph(graph_file)
 
+    matrices = get_boolean_adjacency_matrices(grammar, inverse_grammar, graph, graph_size)
+    remove_terminals(grammar, inverse_grammar)
+
+
+
+def iterate_on_grammar(grammar, inverse_grammar, matrices, shared_memory=False):
+    inverse_by_nonterm = defaultdict(set)
+    for body, heads in inverse_grammar.items():
+        assert type(body) is tuple, 'Left terminals in grammar: {}'.format(body)
+        for head in heads:
+            if body[0] != head:
+                inverse_by_nonterm[body[0]].add((head, body))
+            if body[1] != head:
+                inverse_by_nonterm[body[1]].add((head, body))
+    to_recalculate = products_set(grammar)
+    while to_recalculate:
+        head, body = to_recalculate.pop()
+        assert type(body) is tuple, 'Body is either str or tuple, not {}'.format(type(body))
+        is_changed = ... # Awesome multiplication methods here
+        if not is_changed:
+            continue
+        for product in inverse_by_nonterm[head]:
+            if product != (head, body):
+                to_recalculate.add(product)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
