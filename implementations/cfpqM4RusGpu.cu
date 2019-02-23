e6c25c6ea722d4666332c88a0ef778675b7eb769
NikitaMishin <mishinnikitam@gmail.com>
2019-02-23 12:34:21 +0300

    to camelCase

--- implementations/cfpqM4RusGpu.cu
+++ implementations/cfpqM4RusGpu.cu
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     Grammar grammar = Grammar(argv[1]);
     Graph graph = Graph(argv[2]);
     std::cout << grammar.intersection_with_graph
-             <MethodOf4RusMatrix, MethodOf4RusMatricesEnv>(graph);
+            <MethodOf4RusMatrix, MethodOf4RusMatricesEnv>(graph);
     grammar.print_results(argv[3]);
     return 0;
 }
